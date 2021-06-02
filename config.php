7b0cd73cc4138427c8f6ac584f7c8e4156c7e749
Nipuna Dodantenna <nipunadodan@gmail.com>
2021-06-03 02:25:28 +0530

    unbinding to prevent multiple submits

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ if(DEBUG){
     error_reporting(E_ALL);
 }
 
-define('VERSION', 'v1.0.4.018');
+define('VERSION', 'v1.0.4.019');
 
 define("DOMAIN", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/");
 include_once "env.php";
